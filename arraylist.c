3b60f5ecada25c5afbb5d6f953691c8f5e5ba403
Replit user <>
2023-03-30 20:00:21 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -76,7 +76,7 @@ int get_size(ArrayList * l){
 void clean(ArrayList * l){
   ArrayList *aux = l;
   l = createList();
-  printf("\n Este es size del arraylist : %d\n", l->size);
+  printf("\n Este es size del arraylist : %d\n", l->data[0]);
   free(aux->data);
   free(aux);
 }
